f2393ed4d033a22ecee2fa85cdce0347388aec62
Damaris-Wanjiru <102998560+Damaris-Wanjiru@users.noreply.github.com>
2024-05-13 16:05:04 +0300

    add index.html, javas.js to calculate the area of a trapezium

---
+++ javas.js
@@ -0,0 +1,21 @@
+{
+    let a = 20;
+    let b = 40;
+    let h = 16;
+    let area = (a+b) / 2 * h;
+    console.log(area);
+}
+{
+    let c = 10;
+    let h = 16;
+    let area_triangle = ((c * h) / 2 )* 2;
+
+
+    let l = 20;
+    let area_rectangle = (l * h);
+    let area = area_rectangle + area_triangle
+    console.log(area);
+}
+{
+
+}
